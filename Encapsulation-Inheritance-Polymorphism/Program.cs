7391d69d68ab6973ad329c48a00db218119fa948
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-03-08 16:53:05 +0100

    Add project files.

---
+++ Encapsulation-Inheritance-Polymorphism/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Encapsulation_Inheritance_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(-1, "Khaganovich", "Iluteruminbaigan");
+        }
+    }
+
+    internal class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private int height;
+        private int weight;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                try
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Age must contain a number higher than 0.");
+                    }
+                    else
+                    {
+                        age = value;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public string FName
+        {
+            get { return fName; }
+            set
+            {
+                try
+                {
+                    if (value.Length < 2 || value.Length > 10)
+                    {
+                        throw new ArgumentException("FName must contain at least 2 characters and can't be longer than 10 characters.");
+                    }
+                    else
+                    {
+                        fName = value;
+                    }
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public string LName
+        {
+            get { return lName; }
+            set
+            {
+                try
+                {
+                    if (value.Length < 3 || value.Length > 15)
+                    {
+                        throw new ArgumentException("LName must contain at least 3 characters and can't be longer than 15 characters.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public Person(int age, string fName, string lName)
+        {
+            Age = age;
+            FName = fName;
+            LName = lName;
+        }
+    }
+}
