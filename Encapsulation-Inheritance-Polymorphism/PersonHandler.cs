0d4a0434da7f99068172bc2314b31734e1b23a42
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-03-11 21:34:47 +0100

    Moved Person class to its own file, and added PersonHandler and UserError classes

---
+++ Encapsulation-Inheritance-Polymorphism/PersonHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulation_Inheritance_Polymorphism
+{
+    internal class PersonHandler
+    {
+        public List<Person> persons = new List<Person>();
+
+        public void SetAge(Person pers, int age)
+        {
+            pers.Age = age;
+        }
+
+        public void SetFName(Person pers, string fName)
+        {
+            pers.FName = fName;
+        }
+
+        public void SetLName(Person pers, string lName)
+        {
+            pers.LName = lName;
+        }
+        public void SetHeight(Person pers, int height)
+        {
+            pers.Height = height;
+        }
+        public void SetWeight(Person pers, int weight)
+        {
+            pers.Weight = weight;
+        }
+
+        public static void DisplayPersonalInformation(Person pers)
+        {
+            Console.WriteLine($"Age: {pers.Age}, Full name: {pers.FName} {pers.LName}\nMeasurements: H: {pers.Height}, W: {pers.Weight}");
+        }
+
+        public Person CreatePerson(int age, string fname, string lname, int height, int weight)
+        {
+            Person person = new Person()
+            {
+                Age = age,
+                FName = fname,
+                LName = lname,
+                Height = height,
+                Weight = weight
+            };
+            return person;
+        }
+    }
+}
--- Encapsulation-Inheritance-Polymorphism/Program.cs
+++ Encapsulation-Inheritance-Polymorphism/Program.cs
@@ -1,100 +1,65 @@
-﻿namespace Encapsulation_Inheritance_Polymorphism
+﻿using System;
+
+namespace Encapsulation_Inheritance_Polymorphism
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person(-1, "Khaganovich", "Iluteruminbaigan");
-        }
-    }
-
-    internal class Person
-    {
-        private int age;
-        private string fName;
-        private string lName;
-        private int height;
-        private int weight;
-
-        public int Age
-        {
-            get { return age; }
-            set
+            // 3.1) Encapsulation
+            //1.
+            Person testPerson = new Person()
             {
-                try
-                {
-                    if (value < 0)
-                    {
-                        throw new ArgumentException("Age must contain a number higher than 0.");
-                    }
-                    else
-                    {
-                        age = value;
-                    }
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
-        public string FName
-        {
-            get { return fName; }
-            set
-            {
-                try
-                {
-                    if (value.Length < 2 || value.Length > 10)
-                    {
-                        throw new ArgumentException("FName must contain at least 2 characters and can't be longer than 10 characters.");
-                    }
-                    else
-                    {
-                        fName = value;
-                    }
+                Age = 18,
+                FName = "Test",
+                LName = "Testingsson",
+                Height = 160,
+                Weight = 63
+            };
 
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
-        public string LName
-        {
-            get { return lName; }
-            set
-            {
-                try
-                {
-                    if (value.Length < 3 || value.Length > 15)
-                    {
-                        throw new ArgumentException("LName must contain at least 3 characters and can't be longer than 15 characters.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
-        public int Height
-        {
-            get { return height; }
-            set { height = value; }
-        }
-        public int Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
+            PersonHandler.DisplayPersonalInformation(testPerson);
 
-        public Person(int age, string fName, string lName)
-        {
-            Age = age;
-            FName = fName;
-            LName = lName;
+            //2.
+            PersonHandler personHandler = new PersonHandler();
+            personHandler.SetAge(testPerson, 47);
+            personHandler.SetFName(testPerson, "Hassan");
+            personHandler.SetLName(testPerson, "Shahrazade");
+            personHandler.SetHeight(testPerson, 170);
+            personHandler.SetWeight(testPerson, 73);
+
+            PersonHandler.DisplayPersonalInformation(testPerson);
+
+            //3.
+            Person p1 = personHandler.CreatePerson(34, "Karl", "Gustaf", 180, 82);
+            Person p2 = personHandler.CreatePerson(6, "Yasin", "Köroglu", 112, 82);
+            Person p3 = personHandler.CreatePerson(17, "Daniella", "Lindström", 112, 82);
+            Person p4 = personHandler.CreatePerson(28, "Abebe", "Gebremariam", 169, 64);
+
+            personHandler.persons.Add(p1);
+            personHandler.persons.Add(p2);
+            personHandler.persons.Add(p3);
+            personHandler.persons.Add(p4);
+            
+            //4. & 5.
+            foreach (var person in personHandler.persons)
+            {
+                PersonHandler.DisplayPersonalInformation(person);
+            }
+
+            // 3.2) Polymorphism
+            // 1.
+            List<UserError> userErrors = new List<UserError>();
+            UserError ue1 = new NumericInputError();
+            UserError ue2 = new TextInputError();
+            userErrors.Add(ue1);
+            userErrors.Add(ue2);
+
+            foreach (var ue in userErrors)
+            {
+                Console.WriteLine(ue.UEMessage());
+            }
+
+            
         }
     }
 }
